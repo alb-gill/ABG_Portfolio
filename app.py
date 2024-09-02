fc7e59e2d1367a05e33d13b0a920dbea8e002e4a
Albert Gill <albgill@yahoo.co.uk>
2024-09-02 15:51:22 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+# Core Pkgs
+
+import streamlit as startswith
+
+# Additional Pkgs
+
+
+# Functions
+
+def main():
+    """All our code goes here """
+    st.title("Hello World")
+    
+# if __name__ == '__main__':
